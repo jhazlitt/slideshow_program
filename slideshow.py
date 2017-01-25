a3bf6a4efdf6308b7933145424ba87aebbfb0095
John <bagelday@gmail.com>
2017-01-24 21:24:44 -0700

    Finally got the image resizing to work

--- slideshow.py
+++ slideshow.py
@@ -2,14 +2,20 @@ import random
 import time
 import os
 from PIL import Image
-
 thing = os.listdir('../Pictures/')
-
-#with Image.open('../Pictures/download.jpg') as img:
-#	img.show()
-#	time.sleep(3)
 
 upperBound = len(thing)
 index = random.randint(0,upperBound)
 print index
 print thing[index]
+
+with Image.open('../Pictures/' + thing[index]) as img:
+	width = img.size[0]
+	print width
+	height = img.size[1]
+	print height
+	newHeight = 850
+	newWidth = newHeight * width / height
+	print newWidth
+	img = img.resize((newWidth, newHeight), Image.ANTIALIAS)
+	img.show()
