4a9fd188e422d7591cdeb59cb47317eb5b337305
John <bagelday@gmail.com>
2017-01-24 21:59:50 -0700

    Script pretty much done, could add a sound notification when time is half up, later

--- slideshow.py
+++ slideshow.py
@@ -2,20 +2,32 @@ import random
 import time
 import os
 from PIL import Image
+
+# An initial sketch time can be entered in seconds
+sketchTime = input('Starting sketch time?')
+
 thing = os.listdir('../Pictures/')
 
 upperBound = len(thing)
-index = random.randint(0,upperBound)
-print index
-print thing[index]
 
-with Image.open('../Pictures/' + thing[index]) as img:
-	width = img.size[0]
-	print width
-	height = img.size[1]
-	print height
-	newHeight = 850
-	newWidth = newHeight * width / height
-	print newWidth
-	img = img.resize((newWidth, newHeight), Image.ANTIALIAS)
-	img.show()
+usedIndexes = []
+
+for count in range(upperBound):	
+	# Generate a random index number that has not been used before in the session
+	while True:
+		index = random.randint(0,upperBound)
+		if index not in usedIndexes:
+			break
+	usedIndexes.append(index)
+
+	# Display the image associated with that index number
+	with Image.open('../Pictures/' + thing[index]) as img:
+		width = img.size[0]
+		height = img.size[1]
+		newHeight = 850
+		newWidth = newHeight * width / height
+		img = img.resize((newWidth, newHeight), Image.ANTIALIAS)
+		img.show()
+		time.sleep(sketchTime)
+		# The time allowed to sketch will be decreased by one second with each picture shown
+		sketchTime = sketchTime - 1
